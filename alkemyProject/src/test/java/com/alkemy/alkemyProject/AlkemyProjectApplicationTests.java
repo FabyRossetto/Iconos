7015e778e0a1245046fef2b3c0cf9ea40d70e077
Fabiana Rossetto <fabyross09@gmail.com>
2022-12-19 11:54:48 -0300

    configuracion y db

---
+++ alkemyProject/src/test/java/com/alkemy/alkemyProject/AlkemyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.alkemyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkemyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
